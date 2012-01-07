65c790ac32bac393389293a6e1b8a8f86638fb86
John Hammond <jmhammond@gmail.com>
2012-01-07 13:13:06 -0600

    Cleaned up the code a little bit.

--- src/gui.pyw
+++ src/gui.pyw
@@ -20,15 +20,19 @@ class Form(QDialog):
     def __init__(self, parent=None):
         super(Form, self).__init__(parent)
         
-        self.smses = SmsThread()
+        self.sms_thread = SmsThread()
         
         self.my_html = fileio.read_html("chat.html")
         
         self.browser = QtWebKit.QWebView()
         self.browser.setHtml(self.my_html)
 
-        QtCore.QObject.connect(self.browser.page().mainFrame(), QtCore.SIGNAL("contentsSizeChanged(QSize)"), self.scroll_to_bottom)
-        QtCore.QObject.connect(self.smses, QtCore.SIGNAL("finished_parsing(PyQt_PyObject)"), self.updateUi)
+        QtCore.QObject.connect(self.browser.page().mainFrame(), 
+                               QtCore.SIGNAL("contentsSizeChanged(QSize)"), 
+                               self.scroll_to_bottom)
+        QtCore.QObject.connect(self.sms_thread, 
+                               QtCore.SIGNAL("finished_parsing(PyQt_PyObject)"), 
+                               self.updateUi)
         
         layout = QGridLayout()
         layout.addWidget(self.browser, 0, 0)
@@ -37,15 +41,10 @@ class Form(QDialog):
         self.setWindowTitle("Text Messages")
         
     def start_fetching(self):
-        print("We're starting to fetch")
         try:
-            self.smses.set_credentials()
-            self.smses.set_finished_callback(self.updateUi)
-            self.smses.start()
-            print("Authenticated and whatnot!")
-#            self.timer = QtCore.QTimer()
-#            QtCore.QObject.connect(self.timer, QtCore.SIGNAL("timeout()"), self.refresh_sms)
-#            self.timer.start(15000)
+            self.sms_thread.set_credentials()
+            self.sms_thread.start()
+            QtCore.QTimer.singleShot(15000, form.start_fetching)
         except LoginError, e:
             QMessageBox.warning(self, "Login Error",
                                 "Error logging in: " + unicode(e))
@@ -54,21 +53,18 @@ class Form(QDialog):
         self.browser.page().mainFrame().scroll(size.width(), size.height())
         
     def refresh_sms(self):
-        self.smses.update_sms()
+        self.sms_thread.update_sms()
         self.updateUi()
 
     def updateUi(self, all_msgs):
-        print("Got the message!")
         for msg in all_msgs:
-            self.my_html += "<div class='bubbledRight'>{0}<br/> {1}</div>".format(msg['from'], msg['text'])
+            self.my_html += ("<div class='bubbledRight'>{0}<br/>"
+                            " {1}</div>".format(msg['from'], msg['text']))
         self.browser.setHtml(self.my_html)
  
-            
 
 
-if __name__ == '__main__':
-    #  QSound.play("sound.wav");
-    
+if __name__ == '__main__':    
     app = QApplication(sys.argv)
     form = Form()
     QtCore.QTimer.singleShot(0, form.start_fetching)
--- src/smsthread.py
+++ src/smsthread.py
@@ -1,54 +1,61 @@
 #!/usr/bin/env python
 ###
-# Sms module from pygooglevoice
+# Sms module from 
+# pygooglevoice
 ###
 
 import BeautifulSoup
 from googlevoice import Voice
 from googlevoice import util
-from PyQt4.QtCore import (QThread, QObject, SIGNAL) 
+from PyQt4.QtCore import (QThread, SIGNAL) 
 
 class SmsThread(QThread):
-#   def __init__(self):
         
     def set_credentials(self, email=None, passwd=None):
+        """
+        set_credentials -- establish the login credentials for Google Voice
+        If unspecified, pygooglevoice reverts to looking for ~/.gvoice 
+        """
         self.voice = Voice()
         self.email = email
         self.passwd = passwd
         self.callback = None
 
     def run(self):
+        """
+        run -- called when the thread starts. We process the text messages 
+        and notify any listeners when we're done with 
+        the Qt signal finished_parsing(PyQt_PyObject) 
+        """
         self.voice.login(self.email, self.passwd)
         self.already_read_ids = list()
         self.update_sms()  
         self.emit(SIGNAL("finished_parsing(PyQt_PyObject)"), self.all_msgs)
-        print("Emitted a signal!")
 
     def extract_sms(self, htmlsms):
         """
-        extractsms  --  extract SMS messages from BeautifulSoup tree of Google Voice SMS HTML.
+        extractsms  --  extract SMS messages from BeautifulSoup tree 
+        of Google Voice SMS HTML.
 
         Output is a list of dictionaries, one per message.
         """
-        msgitems = []                                        # accum message items here
-        #    Extract all conversations by searching for a DIV with an ID at top level.
-        tree = BeautifulSoup.BeautifulSoup(htmlsms)            # parse HTML into tree
+        msgitems = []                                        
+     
+        tree = BeautifulSoup.BeautifulSoup(htmlsms)         
         conversations = tree.findAll("div",attrs={"id" : True},recursive=False)
         for conversation in conversations :
-            #    For each conversation, extract each row, which is one SMS message.
+            # For each conversation, extract each row, which is one SMS message.
             rows = conversation.findAll(attrs={"class" : "gc-message-sms-row"})
             for row in rows :                                # for all rows
                 #    For each row, which is one message, extract all the fields.
-                msgitem = {"id" : conversation["id"]}        # tag this message with conversation ID
-                spans = row.findAll("span",attrs={"class" : True}, recursive=False)
-                for span in spans :                            # for all spans in row
+                msgitem = {"id" : conversation["id"]} 
+                spans = row.findAll("span",attrs={"class" : True}, 
+                                    recursive=False)
+                for span in spans :    
                     cl = span["class"].replace('gc-message-sms-', '')
-                    msgitem[cl] = (" ".join(span.findAll(text=True))).strip()    # put text in dict
-                msgitems.append(msgitem)                    # add msg dictionary to list
+                    msgitem[cl] = (" ".join(span.findAll(text=True))).strip()   
+                msgitems.append(msgitem)            
         return msgitems
-
-    def set_finished_callback(self, callback):
-        self.callback = callback
 
     def update_sms(self):
         try:
@@ -62,4 +69,5 @@ class SmsThread(QThread):
                     self.already_read_ids.append(aKey)
                     self.all_msgs.append(msg)
         except util.ParsingError, e:
-            self.all_msgs.append({'from':"", 'time':'12:12', 'text':e, 'id':'12345'})
+            self.all_msgs.append({'from':"", 'time':'12:12', 
+                                  'text':e, 'id':'12345'})
